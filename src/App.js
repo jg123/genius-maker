f484ce9edb208ad7e91e5d27c6d2d433de234d2b
Josh Geller <jgeller@ebsco.com>
2021-04-10 09:56:18 -0400

    Make number of rows dynamic and add required option

--- src/App.js
+++ src/App.js
@@ -4,63 +4,130 @@ import checkWords from "./checkWords";
 import "./App.css";
 
 function App() {
-  const [tempLetters, setTempLetters] = useState({});
-  const [letters, setLetters] = useState([]);
+  const [numberOfletters, setNumberOfLetters] = useState(7);
+  const [tempLetters, setTempLetters] = useState(
+    Array.from({ length: numberOfletters }).fill("")
+  );
+  const [letters, setLetters] = useState(
+    Array.from({ length: numberOfletters }).fill("")
+  );
+  const [requiredLetters, setRequiredLetters] = useState([
+    true,
+    ...Array.from({ length: numberOfletters - 1 }).fill(false)
+  ]);
   const [changed, setChanged] = useState(false);
+  const [toggleRequired, setToggleRequired] = useState(null);
   const [words, setWords] = useState([]);
+
   useEffect(() => {
     if (changed) {
-      setWords(checkWords(letters));
+      setWords(checkWords(letters, requiredLetters));
     }
     setChanged(false);
-  }, [letters, changed]);
+  }, [letters, requiredLetters, changed]);
 
-  const handleChange = event => {
-    setTempLetters({
-      ...tempLetters,
-      ...{ [event.target.name]: event.target.value }
-    });
+  const handleLetterChange = (event, index) => {
+    setTempLetters(tempLetters => [
+      ...tempLetters.slice(0, index),
+      event.target.value,
+      ...tempLetters.slice(index + 1)
+    ]);
   };
+
+  const handleRequiredLetterChange = (event, index) => {
+    setRequiredLetters(currentRequiredLetters => [
+      ...currentRequiredLetters.slice(0, index),
+      event.target.checked,
+      ...currentRequiredLetters.slice(index + 1)
+    ]);
+  };
+
+  let Rows = [];
+  for (let index = 0; index < numberOfletters; index++) {
+    Rows.push(
+      <div className="letter-row" key={index}>
+        <label className="text-label" htmlFor={`letter${index}`}>
+          Letter {index + 1}
+        </label>
+        <input
+          type="text"
+          name={`letter${index}`}
+          id={`letter${index}`}
+          onChange={event => handleLetterChange(event, index)}
+          className="text-input"
+          autoCapitalize="off"
+        />
+        <input
+          className="required-checkbox"
+          type="checkbox"
+          onChange={event => handleRequiredLetterChange(event, index)}
+          checked={requiredLetters[index]}
+          id={`letter${index}required`}
+          name={`letter${index}required`}
+        />
+        <label className="required-label" htmlFor={`letter${index}required`}>
+          required
+        </label>
+      </div>
+    );
+  }
 
   return (
     <>
+      <h1>Genius Maker</h1>
+      <h4>
+        Add/remove rows:
+        <button
+          className="add-remove-button"
+          onClick={() => {
+            setTempLetters([...tempLetters, ""]);
+            setLetters([...letters, ""]);
+            setRequiredLetters([...requiredLetters, false]);
+            setNumberOfLetters(numberOfletters + 1);
+          }}
+        >
+          Add
+        </button>
+        <button
+          className="add-remove-button"
+          onClick={() => {
+            if (numberOfletters < 6) {
+              return;
+            }
+            setTempLetters(tempLetters.slice(0, tempLetters.length - 1));
+            setLetters(letters.slice(0, letters.length - 1));
+            setRequiredLetters(
+              requiredLetters.slice(0, requiredLetters.length - 1)
+            );
+            setNumberOfLetters(numberOfletters - 1);
+          }}
+        >
+          Remove
+        </button>
+      </h4>
+      <h4 className="toggle-row">
+        Toggle required:
+        <button
+          className="add-remove-button"
+          onClick={() => {
+            setRequiredLetters(
+              Array.from({ length: numberOfletters }).fill(!!toggleRequired)
+            );
+            setToggleRequired(!toggleRequired);
+          }}
+        >
+          Toggle
+        </button>
+      </h4>
       <form
         onSubmit={event => {
           event.preventDefault();
           setLetters(tempLetters);
           setChanged(true);
         }}
-        >
-        <h1>Genius Maker</h1>
-        <div class="letter-row">
-          <label class="text-label" for="requiredLetter">Required letter</label>
-          <input type="text" name="requiredLetter" id="requiredLetter" onChange={handleChange} class="text-input" autoCapitalize="off" />
-        </div>
-        <div class="letter-row">
-          <label class="text-label" for="letter2">letter 2</label>
-          <input type="text" name="letter2" id="letter2" onChange={handleChange} class="text-input" autoCapitalize="off" />
-        </div>
-        <div class="letter-row">
-          <label class="text-label" for="letter3">Letter 3</label>
-          <input type="text" name="letter3" id="letter3" onChange={handleChange} class="text-input" autoCapitalize="off" />
-        </div>
-        <div class="letter-row">
-          <label class="text-label" for="letter4">Letter 4</label>
-          <input type="text" name="letter4" id="letter4" onChange={handleChange} class="text-input" autoCapitalize="off" />
-        </div>
-        <div class="letter-row">
-          <label class="text-label" for="letter5">Letter 5</label>
-          <input type="text" name="letter5" id="letter5" onChange={handleChange} class="text-input" autoCapitalize="off" />
-        </div>
-        <div class="letter-row">
-          <label class="text-label" for="letter6">Letter 6</label>
-          <input type="text" name="letter6" id="letter6" onChange={handleChange} class="text-input" autoCapitalize="off" />
-        </div>
-        <div class="letter-row">
-          <label class="text-label" for="letter7">Letter 7</label>
-          <input type="text" name="letter7" id="letter7" onChange={handleChange} class="text-input" autoCapitalize="off" />
-        </div>
-        <input class="submit-button" type="submit" />
+      >
+        {Rows}
+        <input className="submit-button" type="submit" />
       </form>
       <h2>Results:</h2>
       <ol>
